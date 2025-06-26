ff7c6bb1480da645bfd51a57451b50159031f417
Francine <francine_valdivieso@hotmail.com>
2025-06-25 22:54:15 -0300

    Prática 01

---
+++ pratica01/calculadora_soma.py
@@ -0,0 +1,13 @@
+# Calculadora da soma de dois números
+
+# numero1 = int(input("Digite o primeiro número: "))
+# numero2 = int(input("Digite o segundo número: "))
+
+numero1 = 12
+numero2 = 14
+
+# O cálculo da soma 
+soma = numero1 + numero2
+
+# Exibição dos valores
+print(f"A soma de {numero1} e {numer02} é: {soma}")
